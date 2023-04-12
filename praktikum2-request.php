b40353ada127bbba0c9daa7adb32086cba3d336e
Ida Ernawati <125109186+idaaernawati@users.noreply.github.com>
2023-04-12 21:28:16 +0700

    Add files via upload

---
+++ praktikum2-request.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<a href="http://localhost/praktek-6/getAct.php?nama=IDA+AYU+ERNAWATI&email=21082010030%40gmail.com&btnLogin=Login">Link ke Cek</a>
+</body>
+</html>
